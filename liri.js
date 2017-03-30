b4059c19d20da65b7d7c5f06f0d5dce5554d761d
CodingKaiser <falkonoe@live.com>
2017-03-29 18:44:15 -0700

    Got Twitter to work.

--- liri.js
+++ liri.js
@@ -1,37 +1,30 @@
 var keys = require("./keys.js");
 var https = require("https");
+var OAuth = require('oauth');
 
-console.log(keys.twitterKeys);
+var oauth = new OAuth.OAuth(
+	'https://api.twitter.com/oauth/request_token',
+	'https://api.twitter.com/oauth/access_token',
+	keys.twitterKeys.consumer_key,
+	keys.twitterKeys.consumer_secret,          
+	'1.0A',
+	null,
+	'HMAC-SHA1'
+);
 
-var options = {
-	"accept": "*/*",
-	"connection": "close",
-	"method": "GET",
-	"url": "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=falkonoe&count=2",
-	"headers": {
-		"oauth_consumer_key": keys.twitterKeys.consumer_key,
-		"oauth_nonce": Array(32+1).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, 32);
-		"oauth_signature_method": "HMAC-SHA1",
-		"oauth_timestamp": (new Date().getTime() / 1000) + "",
-		"oauth_token": keys.twitterKeys.access_token_key,
-		"oauth_version": "1.0",
+oauth.get(
+	"https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=FalkoJulianNoe&count=20",
+	keys.twitterKeys.access_token_key,
+	keys.twitterKeys.access_token_secret,
+	function (e, data, res) {
+		if (e) console.error(e);
+		JSON.parse(data)
+		JSON.parse(data).forEach(function (tweet) {
+			console.log();
+			console.log("Tweet: '" + tweet.text + "' was created " + tweet.created_at);
+		});
 	}
-}
-
-var randomNumers = ;
-console.log(randomNumers + " is " + randomNumers.length + " characters long");
-
-https.get(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-
-}).on('error', (e) => {
-  console.error(e);
-});
+);
 
 process.argv.forEach(function (val, index, array) {
 	console.log(index + ': ' + val);
